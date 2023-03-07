e7946a724a21d05ab99eca936271b8bee5fed4b2
Miguel <descolbeats@gmail.com>
2023-03-07 11:39:06 +0100

    Formato tabla resultados

--- PruebasFisicasCNP/views.py
+++ PruebasFisicasCNP/views.py
@@ -28,36 +28,43 @@ class Tests():
     def __init__(self):
 
         # Times
-        self.barTime = ""
-        self.circuitTime = ""
-        self.raceTime = ""
-        self.raceTimeMin = ""
-        self.raceTimeSec = ""
+        self.barTime = "-"
+        self.circuitTime = "-"
+        self.raceTime = "-"
+        self.raceTimeMin = "-"
+        self.raceTimeSec = "-"
 
         # Intervals
-        self.barInterval = ""
-        self.circuitInterval = ""
-        self.raceInterval = ""
+        self.barInterval = "-"
+        self.circuitInterval = "-"
+        self.raceInterval = "-"
 
         # Grades
-        self.barGrade = ""
-        self.circuitGrade = ""
-        self.raceGrade = ""
-        self.totalGrade30 = ""
-        self.totalGrade10 = ""
+        self.barGrade = "-"
+        self.circuitGrade = "-"
+        self.raceGrade = "-"
+        self.totalGrade30 = "-"
+        self.totalGrade10 = "-"
 
         # Strings
-        self.strRaceTimeMin = ""
-        self.strRaceTimeSec = ""
+        self.strRaceTime = "-"
+        self.strBarGrade = "-"
+        self.strCircuitGrade = "-"
+        self.strRaceGrade = "-"
+        self.strTotalGrade30 = "-"
+        self.strTotalGrade10 = "-"
 
     def set_bar_time(self, time):
         self.barTime = int(time)
     def set_circuit_time(self, time):
         self.circuitTime = int(time)
     def set_race_time(self, timeMin, timeSec):
-        self.raceTimeMin = int(timeMin)
-        self.raceTimeSec = int(timeSec)
         self.raceTime = int(timeMin) * 60 + int(timeSec)
+        self.raceTimeMin = int(self.raceTime // 60)
+        self.raceTimeSec = int(self.raceTime % 60)
+        strRaceTimeMin = str(self.raceTimeMin)
+        strRaceTimeSec = str(self.raceTimeSec) if self.raceTimeSec>9 else "0" + str(self.raceTimeSec)
+        self.strRaceTime = strRaceTimeMin + ":" + strRaceTimeSec
     def calc_all(self):
         self.calc_bar_grade()
         self.calc_circuit_grade()
@@ -71,19 +78,23 @@ class Tests():
         for i in range(len(Tests.arrBarTimes)):
             if self.barTime >= Tests.arrBarTimes[i]:
                 self.barGrade = i + 1
+        self.strBarGrade
     def calc_circuit_grade(self):
         self.circuitGrade = 0
         for i in range(len(Tests.arrCircuitTimes)):
             if self.circuitTime <= Tests.arrCircuitTimes[i]:
                 self.circuitGrade = i + 1
+        self.strCircuitGrade = str(self.circuitGrade) + "/10"
     def calc_race_grade(self):
         self.raceGrade = 0
         for i in range(len(Tests.arrRaceTimes)):
             if self.raceTime <= Tests.arrRaceTimes[i]:
                 self.raceGrade = i + 1
+        self.strRaceGrade = str(self.raceGrade) + "/10"
     def calc_total_grade(self):
         self.totalGrade30 = self.barGrade + self.circuitGrade + self.raceGrade
         self.totalGrade10 = int(self.totalGrade30/3) if self.totalGrade30%3 == 0 else round(self.totalGrade30/3, 1)
+        self.strTotalGrade = str(self.totalGrade30) + "/30 (" + str(self.totalGrade10) + "/10)"
     
     def calc_bar_interval(self):
         if not self.barGrade:
@@ -106,10 +117,10 @@ class Tests():
             self.raceInterval = f"[{Tests.arrRaceTimes[self.raceGrade-1]//60}:{Tests.arrRaceTimes[self.raceGrade-1]%60} - {Tests.arrRaceTimes[self.raceGrade]//60}:{Tests.arrRaceTimes[self.raceGrade]%60-1}]"
         else:
             self.raceInterval = f"[0 - {Tests.arrRaceTimes[9]//60}:{Tests.arrRaceTimes[9]%60}]"
-    def set_strings(self):
+    """ def set_strings(self):
         strRaceTimeMin = str(self.raceTimeMin)
         strRaceTimeSec = str(self.raceTimeSec) if self.raceTimeSec>9 else "0"+str(self.raceTimeSec)
-        self.strRaceTime = strRaceTimeMin+":"+strRaceTimeSec
+        self.strRaceTime = strRaceTimeMin+":"+strRaceTimeSec """
 
 def view_home(request):    
     if request.method != "POST":
@@ -127,7 +138,6 @@ def view_home(request):
         tests.set_circuit_time(request.POST['timeCircuit'])
         tests.set_race_time(request.POST['timeRaceMin'], request.POST['timeRaceSec'])
         tests.calc_all()
-        tests.set_strings()
     return render(request, 'home.html', {"tests":tests})
 
 """ def view_buscar(request):
