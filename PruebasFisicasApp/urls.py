972e08282bd0f336978b110bef00e9a5dd3aaf12
Miguel <descolbeats@gmail.com>
2023-03-08 12:34:42 +0100

    Add Bottstrap y herencia de plantillas

--- PruebasFisicasApp/urls.py
+++ PruebasFisicasApp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from PruebasFisicasApp import views
 
 urlpatterns = [
-    path('home/', views.view_home),
+    path('home/', views.view_home, name="Home"),
     path('bootstrap/', views.bootstrap),
 ]
